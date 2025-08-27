2f0cbbf02803a38ccceeb2bf436f21527667df87
Jean-François Meurgey <jfm.data01@gmail.com>
2025-08-27 11:37:51 +0000

    merge jf bapt en sql

--- models/intermediate/restau_merged_jeff_bapt.sql
+++ models/intermediate/restau_merged_jeff_bapt.sql
@@ -1,4 +1,6 @@
-SELECT r.*,i.Type_principal_normal
+SELECT
+r.*,
+i.Type_principal_normal
 FROM {{ ref('stg_RESTAUPARIS__RestauParis_new_cat5') }} as r
 LEFT JOIN {{ ref('int_bapt_categories') }} as i
 ON r.adresse_complete = lower(i.adresse_complete)
