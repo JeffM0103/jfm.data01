cca17d919d9adcd699c131b1b0397778a48783be
Baptiste VIU <baptiste.data@gmail.com>
2025-08-27 15:37:58 +0000

    commit remplace 2

--- models/intermediate/restau_merged_jeff_bapt_abde.sql
+++ models/intermediate/restau_merged_jeff_bapt_abde.sql
@@ -3,4 +3,4 @@ a.*,
 b.d__tail_restaurant_2
 FROM {{ ref('restau_remplace_1') }} as a
 LEFT JOIN {{ ref('stg_RESTAUPARIS__cat_restau_abdellah') }} as b
-ON a.adresse_complete = lower(b.adresse_complete)
+ON lower(a.adresse_complete) = lower(b.adresse_complete)
