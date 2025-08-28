482ca87937c9aebc574e0ec5216a7d5ff9d1cbb2
Jean-François Meurgey <jfm.data01@gmail.com>
2025-08-28 15:42:48 +0000

    bool corrigés

--- models/intermediate/restau_merged_INSEE.sql
+++ models/intermediate/restau_merged_INSEE.sql
@@ -2,109 +2,73 @@ select
     r.*,
     case
         when
-            regexp_contains(
-                coalesce(
-                    lower(r.nom),
-                    lower(r.description),
-                    lower(r.type_principal),
-                    lower(r.tous_les_types)
-                ),
-                r'gastronomique'
-            )
+            regexp_contains(lower(r.nom), r'gastronomique')
+        or regexp_contains(lower(r.description), r'gastronomique')
+        or regexp_contains(lower(r.type_principal), r'gastronomique')
+        or regexp_contains(lower(r.tous_les_types), r'gastronomique')
         then true
         else false
     end as gastronomique,
     case
-        when
-            regexp_contains(
-                coalesce(
-                    lower(r.nom),
-                    lower(r.description),
-                    lower(r.type_principal),
-                    lower(r.tous_les_types)
-                ),
-                r'restauration rapide'
-            )
-        then true
-        else false
-    end as restauration_rapide,
+    when
+        regexp_contains(lower(r.nom), r'restauration rapide')
+        or regexp_contains(lower(r.description), r'restauration rapide')
+        or regexp_contains(lower(r.type_principal), r'restauration rapide')
+        or regexp_contains(lower(r.tous_les_types), r'restauration rapide')
+    then true
+    else false
+end as restauration_rapide,
 
     case
         when
-            regexp_contains(
-                coalesce(
-                    lower(r.nom),
-                    lower(r.description),
-                    lower(r.type_principal),
-                    lower(r.tous_les_types)
-                ),
-                r' fusion'
-            )
+            regexp_contains(lower(r.nom), r' fusion')
+        or regexp_contains(lower(r.description), r' fusion')
+        or regexp_contains(lower(r.type_principal), r' fusion')
+        or regexp_contains(lower(r.tous_les_types), r' fusion')
         then true
         else false
     end as cuisine_fusion,
     case
         when
-            regexp_contains(
-                coalesce(
-                    lower(r.nom),
-                    lower(r.description),
-                    lower(r.type_principal),
-                    lower(r.tous_les_types)
-                ),
-                r'brasserie|bistro '
-            )
+           regexp_contains(lower(r.nom), r'brasserie|bistro')
+        or regexp_contains(lower(r.description), r'brasserie|bistro')
+        or regexp_contains(lower(r.type_principal), r'brasserie|bistro')
+        or regexp_contains(lower(r.tous_les_types), r'brasserie|bistro')
         then true
         else false
     end as brasserie,
 
     case
         when
-            regexp_contains(
-                coalesce(
-                    lower(r.nom),
-                    lower(r.description),
-                    lower(r.type_principal),
-                    lower(r.tous_les_types)
-                ),
-                r'grillade'
-            )
+        regexp_contains(lower(r.nom), r'grillade')
+        or regexp_contains(lower(r.description), r'grillade')
+        or regexp_contains(lower(r.type_principal), r'grillade')
+        or regexp_contains(lower(r.tous_les_types), r'grillade')
         then true
         else false
     end as grillade,
     case
         when
-            regexp_contains(
-                coalesce(
-                    lower(r.nom),
-                    lower(r.description),
-                    lower(r.type_principal),
-                    lower(r.tous_les_types)
-                ),
-                r' tapas'
-            )
+            regexp_contains(lower(r.nom), r'tapas')
+        or regexp_contains(lower(r.description), r'tapas')
+        or regexp_contains(lower(r.type_principal), r'tapas')
+        or regexp_contains(lower(r.tous_les_types), r'tapas')
         then true
         else false
     end as tapas,
     case
         when
-            regexp_contains(
-                coalesce(
-                    lower(r.nom),
-                    lower(r.description),
-                    lower(r.type_principal),
-                    lower(r.tous_les_types)
-                ),
-                r'[aà] volonté'
-            )
+            regexp_contains(lower(r.nom), r'[aà] volonté')
+        or regexp_contains(lower(r.description), r'[aà] volonté')
+        or regexp_contains(lower(r.type_principal), r'[aà] volonté')
+        or regexp_contains(lower(r.tous_les_types), r'[aà] volonté')
         then true
         else false
     end as a_volonte,
     case
         when
-            regexp_contains(
-                coalesce(lower(r.type_principal), lower(r.tous_les_types)), r'bar'
-            )
+           regexp_contains(lower(r.type_principal), r'bar')
+        or regexp_contains(lower(r.tous_les_types), r'bar')
         then true
         else false
     end as bar,
